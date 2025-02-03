840edeeaa9cd6642b3063d0eeab0eea15572a444
Mescxll <202411190002@ifba.edu.br>
2025-02-03 13:32:41 -0300

    Atualização nos comentários e modificações de Hardware

--- temporizador_periodico/clock_semaforo.c
+++ temporizador_periodico/clock_semaforo.c
@@ -1,58 +1,59 @@
-//Tarefa U4C5 feita por Maria Eduarda Santos Campos
+//Tarefa U4C5 feita por Maria Eduarda Santos Campos.
 
-#include <stdio.h> //Biblioteca padrão do C
-#include "pico/stdlib.h"     // Biblioteca padrão para funções básicas de E/S, temporização, controle de GPIO, etc.
-#include "hardware/clocks.h" // Biblioteca para configuração e gerenciamento dos clocks do microcontrolador RP2040.
-#include "hardware/pll.h"    // Biblioteca para controle e configuração dos Phase-Locked Loops (PLLs).
+#include <stdio.h> 
+#include "pico/stdlib.h"     
+#include "hardware/clocks.h" 
+#include "hardware/pll.h"    
 
-#define LED_VM 11
-#define LED_AM 12
-#define LED_VD 13
+//LEDs vermelho, amarelop e verde
+#define LED_VM 13 //Na placa BitDogLab, led verde
+#define LED_AM 12 //Na placa BitDogLab, led azul
+#define LED_VD 11 //Na placa BitDogLab, led vermelho
 
-
-
-// Função de callback que será chamada repetidamente pelo temporizador
-// O tipo bool indica que a função deve retornar verdadeiro ou falso para continuar ou parar o temporizador.
+// Função de callback
 bool repeating_timer_callback(struct repeating_timer *t) {
+    //Liga a amarela e desliga o resto
     if(gpio_get(LED_VM) == 1&&gpio_get(LED_VD) == 0&&gpio_get(LED_AM) == 0){
         gpio_put(LED_VM, false);
         gpio_put(LED_AM, true); 
         gpio_put(LED_VD, false);
-    }else if(gpio_get(LED_VM) == 0&&gpio_get(LED_VD) == 0&&gpio_get(LED_AM) == 1){
+    }else //Liga a verde e desliga o resto
+    if(gpio_get(LED_VM) == 0&&gpio_get(LED_VD) == 0&&gpio_get(LED_AM) == 1){
         gpio_put(LED_VM, false);
         gpio_put(LED_AM, false);        
         gpio_put(LED_VD, true);
-    }else if(gpio_get(LED_VM) == 0&&gpio_get(LED_VD) == 1&&gpio_get(LED_AM) == 0){
+    }else //Liga a vermelha e desliga o resto
+    if(gpio_get(LED_VM) == 0&&gpio_get(LED_VD) == 1&&gpio_get(LED_AM) == 0){
         gpio_put(LED_VM, true); 
         gpio_put(LED_AM, false);
         gpio_put(LED_VD, false);
     }
-    return true;
+    return true; //Retorna verdadeiro ou falso para continuar ou parar o temporizador.
 }
 
-int main() {//função principal
-    // Inicializa a comunicação serial, permitindo o uso das funções padrão de E/S como printf.
+//Função principal
+int main() {
     stdio_init_all();
 
-    //Configura a frequência do clock do sistema (clk_sys) para um valor específico em kHz
-    //set_sys_clock_khz(100000, false);
-
+    //Inicializa e define os LEDs como saída
     gpio_init(LED_VM);
     gpio_init(LED_AM);
     gpio_init(LED_VD);
     gpio_set_dir(LED_VM, true);
     gpio_set_dir(LED_AM, true);
     gpio_set_dir(LED_VD, true);
-    // Declaração de uma estrutura de temporizador de repetição.
-    // Esta estrutura armazenará informações sobre o temporizador configurado.
+    
+    //Liga o LED vermelho inicialamente
     gpio_put(LED_VM, true);
 
+    //Estrutura de temporizador de repetição que armazena informações sobre o temporizador configurado.
     struct repeating_timer timer;
 
-    // Configura o temporizador para chamar a função de callback a cada 1 segundo.
+    //Chama a função de callback a cada três segundos
     add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
 
     while (true){
+        //Mostra no terminal quantos segundos se passaram de 3 em 3 segundos
         printf("Um segundo se passou\n");
         sleep_ms(1000);
         printf("Dois segundos se passaram\n");
@@ -61,6 +62,5 @@ int main() {//função principal
         sleep_ms(1000);
         printf(" \n");
     }
-
     return 0;
 }
